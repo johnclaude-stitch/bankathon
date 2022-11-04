e003c950e56fd131a413d58a71d76ae00a66aab1
Un John Claude <john@stitch.money>
2022-11-04 15:10:17 +0200

    DRY

--- absa/login.spec.ts
+++ absa/login.spec.ts
@@ -1,45 +1,11 @@
 import { test, expect, chromium, Page } from "@playwright/test";
+import { login } from "../modules/login";
 
-test("absa", async ({ page }) => {
+test.only("absa", async ({ page }) => {
   await page.goto("https://ib.absa.co.za/absa-online/login.jsp");
 
-  // Inputs fields
-  const usernameId = "j_username";
-  const pinId = "j_pin";
-  const userNumberId = "j_user_no";
-
-  // Input values
-  let accountNumber = "4082314247";
-  let pin = "2712";
-  let userNumber = "1";
-  let userPassword = "bigboygpr250";
-
-  // Insert username/acct number
-  await page.waitForSelector("#j_username");
-  await page.locator(`#j_username`).fill(accountNumber);
-
-  await page.waitForSelector("#j_pin");
-  await page.locator(`#j_pin`).fill(pin);
-
-  await page.waitForSelector("#j_user_no");
-  await page.locator(`#j_user_no`).fill(userNumber);
-
-  await page.getByRole("button", { name: "Next" }).click();
-
-  // // Enter password
-  await page.waitForSelector(".pf");
-  const arrayOfPasswords = await page.locator(".pf");
-  const elementsCount = await arrayOfPasswords.count();
-
-  for (let index = 0; index < elementsCount; index++) {
-    const element = await arrayOfPasswords.nth(index);
-    if (!(await element.isDisabled())) {
-      await element.fill(userPassword[index]);
-    }
-  }
-
-  await page.getByRole("button", { name: "Logon" }).click();
-  await page.waitForLoadState('domcontentloaded');
+  // login
+  await login(page);
 
   await expect(page).toHaveURL("https://ib.absa.co.za/absa-online/login.jsp");
 });
